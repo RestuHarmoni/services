30ef5af1582d5d76a78ac798a8463bb442635915
RestuHarmoni <restuharmoni@gmail.com>
2026-06-16 10:58:32 +0800

    Delete pakej directory

--- pakej/rh-basic/README.md
+++
@@ -1,3 +0,0 @@
-# RH Basic Website Template
-
-Basic landing/profile package for personal web and small industries.
--- pakej/rh-growth/assets/script.js
+++
@@ -1,2 +0,0 @@
-const toggle=document.querySelector('.nav-toggle');const menu=document.querySelector('.nav-menu');toggle?.addEventListener('click',()=>menu.classList.toggle('open'));
-const reveal=()=>document.querySelectorAll('.reveal').forEach(el=>{if(el.getBoundingClientRect().top<innerHeight-80)el.classList.add('show')});addEventListener('scroll',reveal);reveal();
